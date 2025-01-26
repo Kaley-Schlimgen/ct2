33460f67ce5b0fd33ff37d043ce9a7b123060c93
Kaley-Schlimgen <kaleynicole89@gmail.com>
2025-01-26 12:13:02 -0700

    back to string?

--- ct2/main.cpp
+++ ct2/main.cpp
@@ -6,41 +6,36 @@
 //
 
 #include <iostream>
+#include <cstring>
 #include <string>
 #include <vector>
 
 int main() {
-    std::string lastName;
-    std::string firstName;
-//FIXME change to 3
-    //numPatients is 3
-    int numPatients = 2;
+    char firstWord[200] = "";
+    char secondWord[200] = "";
     
-    //create vector so we can get and print the user's input using a for loop
-    std::vector<std::string> fullName(numPatients);
+    int numWords = 3;
     
-    //get two string user inputs 3 (numPatients = 3) times
-    for (int i = 0; i < numPatients; ++i) {
-        //take two string inputs from user
-        std::cout << "Enter Patient " << i + 1 << "'s Last Name: ";
-        std::getline(std::cin, lastName);
+    std::cout <<"BUILDING COMPOUND WORDS\n";
+    
+    //get two string user inputs, numWords times
+    for (int i = 0; i < numWords; ++i) {
+        std::cout << "\nWORD #" << i + 1;
         
-        std::cout << "Enter Patient " << i + 1 << "'s First Name: ";
-        std::getline(std::cin, firstName);
+        //take two string inputs from user
+        std::cout << "\nEnter First Word of Compound Word #" << i + 1 << ": ";
+        std::cin.getline(firstWord, 200);
 
-//FIXME should names be smooshed together?
-//FIXME would separating by comma not be concatenation?
-        //concatenate the two strings
-        fullName[i] = lastName + firstName;
+        
+        std::cout << "Enter Second Word of Compound Word #" << i + 1 << ": ";
+        std::cin.getline(secondWord, 200);
+        
+        std::strcat(firstWord, secondWord);
+        std::cout << "Your Compound Word is: " << firstWord << std::endl;
+        
     }
     
-    std::cout << "\nPATIENT OUTPUT:\n";
-    
-    //print concatenated inputs of various lengths
-    for (int i = 0; i < numPatients; ++i) {
-        //print concatenate of two entered strings as output onto screen
-        std::cout << "Patient " << i + 1 << ": " << fullName[i] << std::endl;
-    }
+    std::cout << std::endl;
      
     return 0;
 }
